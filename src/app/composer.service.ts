0f222cb21f87a3f66280861de6d5c9644b63a26b
Walter McCue <walter.mccue@gmail.com>
2023-01-29 13:36:30 -0500

    init

---
+++ src/app/composer.service.ts
@@ -0,0 +1,59 @@
+/**
+ * Title: composer.service.ts
+ * Author: Walter McCue
+ * Date: 01/28/23
+ * Description: creates the composer class for the di-composer-app project
+*/
+
+// import statement
+
+import { Injectable } from '@angular/core';
+import { IComposer } from './composer.interface';
+import { Observable, of, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComposerService {
+
+  composers: Array<IComposer>;
+  constructor() {
+    this.composers = [
+      {
+        composerId: 100, fullName: "Ludwig van Beetoven", genre: "Classical"
+      },
+      {
+        composerId: 101, fullName: "Johann Sebastian Bach", genre: "Classical"
+      },
+      {
+        composerId: 102, fullName: "Wolfgang Amadeus Mozart", genre: "Classical"
+      },
+      {
+        composerId: 103, fullName: "Johannes Brahms", genre: "Classical"
+      },
+      {
+        composerId: 104, fullName: "Joseph Haydn", genre: "Classical"
+      },
+      {
+        composerId: 105, fullName: "Nobuo Uematsu", genre: "Modern Orchestral"
+      }
+    ]
+  }
+
+  getComposers(): Observable<IComposer[]> {
+    return of(this.composers);
+  }
+
+  getComposer(composerId: number) {
+    for (let composer of this.composers) {
+      if (composer.composerId === composerId) {
+        return composer;
+      }
+    }
+    return {} as IComposer;
+  }
+
+  filterComposers(name: string): Observable<IComposer[]> {
+    return of(this.composers).pipe(map(composers => composers.filter(composer => composer.fullName.toLowerCase().indexOf(name)>-1)));
+  }
+}
